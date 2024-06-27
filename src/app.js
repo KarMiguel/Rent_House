451a0fa554946656f994f5ac0cf459af0e68d4b5
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-27 17:23:11 -0300

    add config CORS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import routes from './routes';
 import mongoose from 'mongoose';
+import routes from './routes';
+import path from 'path'; 
+import cors from 'cors'
+
 
 class App {
+
     constructor() {
         this.server = express();
         this.middlewares();
@@ -17,8 +22,15 @@ class App {
     }
 
     middlewares() {
+
+        this.server.use(cors())
+
+        this.server.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'uploads'))
+        );
         this.server.use(express.json());
-    }
+ }
 
     routes() {
         this.server.use(routes);
