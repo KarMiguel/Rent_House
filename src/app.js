19b01233d12ba880603374f55561007c050c945d
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-27 10:16:49 -0300

    feat: :sparkles: init config

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import routes from './routes';
+import mongoose from 'mongoose';
+
+class App {
+    constructor() {
+        this.server = express();
+        this.middlewares();
+        this.routes();
+
+        mongoose.connect('mongodb+srv://karMiguel:adminpass@cluster0.dcsjhhz.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0', {
+        }).then(() => {
+            console.log('Database connection successful');
+        }).catch((err) => {
+            console.error('Database connection error:', err);
+        });
+    }
+
+    middlewares() {
+        this.server.use(express.json());
+    }
+
+    routes() {
+        this.server.use(routes);
+    }
+}
+
+export default new App().server;
