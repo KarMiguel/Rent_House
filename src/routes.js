07acf7bb0aa4421a46f2b1839ab85008fcab2196
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-27 17:22:54 -0300

    adding routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,34 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import { specs, swaggerUi } from './config/swaggerConfig';
+import multer from 'multer';
+import uploadConfig from './config/uploadConfig';
+
+
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
-
-const routes = new Router()
-
-routes.post('/sessions',SessionController.store);
-routes.post('/houses',HouseController.store);
+import DashboardController from './controllers/DashboardController';
+import ReserveController from './controllers/ReserveController';
 
 
-export default  routes;
+
+const routes = new Router();
+const upload = multer(uploadConfig);
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(specs));
+
+routes.post('/sessions', SessionController.store);
+
+routes.post('/houses', upload.single('thumbnail'), HouseController.store);
+routes.get('/houses',HouseController.index);
+routes.put('/houses/:house_id',upload.single('thumbnail'), HouseController.store)
+routes.delete('/houses', HouseController.destroy);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/houses/:house_id/reserve', ReserveController.store);
+routes.get('/reserves', ReserveController.index);
+routes.delete('/reserves/cancel', ReserveController.destroy);
+
+
+export default routes;
