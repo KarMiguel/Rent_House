2729560bb81b621445762dc01fe6f18c9838f92d
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-28 16:17:05 -0300

    change app

--- src/app.js
+++ src/app.js
@@ -2,39 +2,39 @@ import express from 'express';
 import routes from './routes';
 import mongoose from 'mongoose';
 import routes from './routes';
-import path from 'path'; 
-import cors from 'cors'
+import path from 'path';
+import cors from 'cors';
 
 
 class App {
 
-    constructor() {
-        this.server = express();
-        this.middlewares();
-        this.routes();
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
 
-        mongoose.connect('mongodb+srv://karMiguel:adminpass@cluster0.dcsjhhz.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0', {
-        }).then(() => {
-            console.log('Database connection successful');
-        }).catch((err) => {
-            console.error('Database connection error:', err);
-        });
-    }
+    mongoose.connect('mongodb+srv://karMiguel:adminpass@cluster0.dcsjhhz.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0', {
+    }).then(() => {
+      console.log('Database connection successful');
+    }).catch((err) => {
+      console.error('Database connection error:', err);
+    });
+  }
 
-    middlewares() {
+  middlewares() {
 
-        this.server.use(cors())
+    this.server.use(cors())
 
-        this.server.use(
-            '/files',
-            express.static(path.resolve(__dirname, '..', 'uploads'))
-        );
-        this.server.use(express.json());
- }
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'uploads'))
+    );
+    this.server.use(express.json());
+  }
 
-    routes() {
-        this.server.use(routes);
-    }
+  routes() {
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
