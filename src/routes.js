f1e8dd4446ff1d95aa5c6213d82acaa802d7eaf8
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-06-29 19:21:31 -0300

    :sparkles: fix: adding routes

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,13 @@ import { specs, swaggerUi } from './config/swaggerConfig';
 import multer from 'multer';
 import uploadConfig from './config/uploadConfig';
 
+import UserController from './app/controllers/UserController';
+import HouseController from './app/controllers/HouseController';
+import DashboardController from './app/controllers/DashboardController';
+import ReserveController from './app/controllers/ReserveController';
+import SessionController from './app/controllers/SessionController';
 
-import SessionController from './controllers/SessionController';
-import HouseController from './controllers/HouseController';
-import DashboardController from './controllers/DashboardController';
-import ReserveController from './controllers/ReserveController';
-
-
+import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -17,11 +17,17 @@ const upload = multer(uploadConfig);
 routes.use('/api-docs', swaggerUi.serve);
 routes.get('/api-docs', swaggerUi.setup(specs));
 
+routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/sessions/forgot_password', SessionController.forgotPassword);
+routes.post('/sessions/reset_password', SessionController.resetPassword);
+
+routes.use(authMiddleware);
+
 
 routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 routes.get('/houses',HouseController.index);
-routes.put('/houses/:house_id',upload.single('thumbnail'), HouseController.store)
+routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses', HouseController.destroy);
 
 routes.get('/dashboard', DashboardController.show);
